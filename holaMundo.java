3d3a2015ae7696456f53e3e521fc03028b51fc33
Diego-08 <diegomartinez31@aragon.unam.mx>
2020-07-30 18:19:18 -0500

    Multiplicador en java

--- holaMundo.java
+++ holaMundo.java
@@ -1,6 +1,14 @@
 public class holaMundo{
 
-	public static void main (String [] )
-		
-		System.out.print("Hola Mundo")
+	public static void main (String [] args){
+	int a,b,c;	
+		System.out.print("Hola Mundo");
+	//Tablas de multiplicar 5
+
+		b=5;
+		for(int a=0; a<10; a++){
+			
+			c=a*b;
+	}
+
 }
